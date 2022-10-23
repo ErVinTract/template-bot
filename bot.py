7e93939e9b8419b2cee1dd32378c38662f04f3cd
Daniil <86712293+ErVinTract@users.noreply.github.com>
2022-10-23 18:40:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from tgbot.config import load_config
 from tgbot.filters.role import RoleFilter, AdminFilter
@@ -52,7 +52,7 @@ async def main():
     config = load_config("bot.ini")
 
     if config.tg_bot.use_redis:
-        storage = RedisStorage()
+        storage = RedisStorage2()
     else:
         storage = MemoryStorage()
 
